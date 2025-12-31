596063cc9a59aa60d6fe53fd322293ea83cc72aa
Naveen khatri <naveen1khatri@gmail.com>
2026-01-01 01:32:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const text = document.getElementById("text");
+const circle = document.querySelector(".circle");
 
+function playAnim() {
+  circle.style.animationPlayState = "running";
+  text.style.animationPlayState = "running";
+}
+
+function resetAnim() {
+  circle.style.animation = "none";
+  text.style.animation = "none";
+
+  setTimeout(() => {
+    circle.style.animation = "rotate 6s linear infinite";
+    text.style.animation = "glow 2s ease-in-out infinite alternate";
+  }, 100);
+}
